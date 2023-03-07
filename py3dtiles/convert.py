fix(las_reader): correct color_scale calculation
we used to test if one dimension was above 256 for the first
10000 points and see if we need a color scale but it seems to me that
laspy always returns 16bits colors.  the old logic was faulty
because if you converted 2 files, one with black points and the
other with colors you'd have 50% chance to have everything black

py3dtiles/convert.py
@@ -77,6 +77,7 @@ class Worker(Process):
         octree_metadata: OctreeMetadata,
         folder: Path,
         write_rgb: bool,
+        color_scale: Optional[float],
         write_classification: bool,
         verbosity: int,
         uri: str,
@@ -87,6 +88,7 @@ class Worker(Process):
         self.octree_metadata = octree_metadata
         self.folder = folder
         self.write_rgb = write_rgb
+        self.color_scale = color_scale
         self.write_classification = write_classification
         self.verbosity = verbosity
         self.uri = uri
@@ -186,6 +188,7 @@ class Worker(Process):
             parameters["portion"],
             self.skt,
             self.transformer,
+            self.color_scale,
         )
 
     def execute_write_pnts(self, content):
@@ -424,7 +427,7 @@ class _Convert:
         :param rgb: Export rgb attributes.
         :param classification: Export classification attribute.
         :param graph: Produce debug graphs (requires pygal).
-        :param color_scale: Force color scale
+        :param color_scale: Scale the color with the specified amount. Useful to lighten or darken black pointclouds with only intensity.
 
         :raises SrsInMissingException: if py3dtiles couldn't find srs informations in input files and srs_in is not specified
         :raises SrsInMixinException: if the input files have different CRS
@@ -444,7 +447,9 @@ class _Convert:
         self.benchmark = benchmark
         self.startup = None
 
-        self.file_info = self.get_file_info(color_scale, crs_in, force_crs_in)
+        self.color_scale = color_scale
+
+        self.file_info = self.get_file_info(crs_in, force_crs_in)
 
         transformer = self.get_transformer(crs_out)
         (
@@ -484,6 +489,7 @@ class _Convert:
                 octree_metadata,
                 self.out_folder,
                 self.rgb,
+                self.color_scale,
                 self.classification,
                 self.verbose,
             ),
@@ -493,14 +499,12 @@ class _Convert:
 
     def get_file_info(
         self,
-        color_scale: Optional[float],
         crs_in: Optional[CRS],
         force_crs_in: bool = False,
     ) -> Dict[str, Any]:
 
         pointcloud_file_portions = []
         aabb = None
-        color_scale_by_file = {}
         total_point_count = 0
         avg_min = np.array([0.0, 0.0, 0.0])
 
@@ -515,7 +519,7 @@ class _Convert:
                     f"the available extensions are: {READER_MAP.keys()}"
                 )
 
-            file_info = reader.get_metadata(file, color_scale=color_scale)
+            file_info = reader.get_metadata(file)
 
             pointcloud_file_portions += file_info["portions"]
             if aabb is None:
@@ -523,7 +527,6 @@ class _Convert:
             else:
                 aabb[0] = np.minimum(aabb[0], file_info["aabb"][0])
                 aabb[1] = np.maximum(aabb[1], file_info["aabb"][1])
-            color_scale_by_file[str(file)] = file_info["color_scale"]
 
             file_crs_in = str_to_CRS(file_info["srs_in"])
             if file_crs_in is not None:
@@ -540,7 +543,6 @@ class _Convert:
         return {
             "portions": pointcloud_file_portions,
             "aabb": aabb,
-            "color_scale": color_scale_by_file,
             "crs_in": crs_in,
             "point_count": total_point_count,
             "avg_min": avg_min,
@@ -877,9 +879,6 @@ class _Convert:
                             self.root_scale,
                             self.rotation_matrix[:3, :3].T
                             if self.rotation_matrix is not None
-                            else None,
-                            self.file_info["color_scale"].get(file)
-                            if self.file_info["color_scale"] is not None
                             else None,
                         ),
                         "portion": portion,

py3dtiles/reader/las_reader.py
@@ -15,25 +15,13 @@ from py3dtiles.typing import MetadataReaderType, OffsetScaleType, PortionType
 from py3dtiles.utils import ResponseType
 
 
-def get_metadata(
-    path: Path, color_scale: Optional[float] = None, fraction: int = 100
-) -> MetadataReaderType:
+def get_metadata(path: Path, fraction: int = 100) -> MetadataReaderType:
     pointcloud_file_portions = []
     srs_in = None
 
     filename = str(path)
     with laspy.open(filename) as f:
         point_count = f.header.point_count * fraction // 100
-
-        # read the first points red channel
-        if not color_scale:
-            if "red" in f.header.point_format.dimension_names:
-                points = next(f.chunk_iterator(10_000))["red"]
-                if np.max(points) > 255:
-                    color_scale = 1.0 / 255
-            else:
-                # the intensity is then used as color
-                color_scale = 1.0 / 255
 
         _1M = min(point_count, 1_000_000)
         steps = math.ceil(point_count / _1M)
@@ -49,7 +37,6 @@ def get_metadata(
     return {
         "portions": pointcloud_file_portions,
         "aabb": np.array([f.header.mins, f.header.maxs]),
-        "color_scale": color_scale,
         "srs_in": srs_in,
         "point_count": point_count,
         "avg_min": np.array(f.header.mins),
@@ -62,6 +49,7 @@ def run(
     portion: PortionType,
     queue: Socket,
     transformer: Optional[Transformer],
+    color_scale: Optional[float],
 ) -> None:
     """
     Reads points from a las file
@@ -74,8 +62,6 @@ def run(
             step = min(point_count, max(point_count // 10, 100_000))
 
             indices = list(range(math.ceil(point_count / step)))
-
-            color_scale = offset_scale[3]
 
             for index in indices:
                 start_offset = portion[0] + index * step
@@ -114,14 +100,14 @@ def run(
                     green = points["intensity"]
                     blue = points["intensity"]
 
-                if not color_scale:
-                    red = red.astype(np.uint8)
-                    green = green.astype(np.uint8)
-                    blue = blue.astype(np.uint8)
-                else:
+                if color_scale:
                     red = (red * color_scale).astype(np.uint8)
                     green = (green * color_scale).astype(np.uint8)
                     blue = (blue * color_scale).astype(np.uint8)
+                else:
+                    red = red.astype(np.uint8)
+                    green = green.astype(np.uint8)
+                    blue = blue.astype(np.uint8)
 
                 colors = np.vstack((red, green, blue)).transpose()
 

py3dtiles/reader/ply_reader.py
@@ -13,9 +13,7 @@ from py3dtiles.typing import MetadataReaderType, OffsetScaleType, PortionType
 from py3dtiles.utils import ResponseType
 
 
-def get_metadata(
-    path: Path, color_scale: Optional[float] = None, fraction: int = 100
-) -> MetadataReaderType:
+def get_metadata(path: Path, fraction: int = 100) -> MetadataReaderType:
     """Get metadata in case of a input ply file."""
     ply_point_cloud = PlyData.read(path)
     if "vertex" not in [e.name for e in ply_point_cloud.elements]:
@@ -40,7 +38,6 @@ def get_metadata(
     return {
         "portions": pointcloud_file_portions,
         "aabb": aabb,
-        "color_scale": color_scale,
         "srs_in": None,
         "point_count": point_count,
         "avg_min": aabb[0],
@@ -53,6 +50,7 @@ def run(
     portion: PortionType,
     queue: Socket,
     transformer: Optional[Transformer],
+    color_scale: Optional[float],
 ) -> None:
     """
     Reads points from a ply file.
@@ -64,7 +62,6 @@ def run(
         point_count = portion[1] - portion[0]
         step = min(point_count, max(point_count // 10, 100_000))
         indices = list(range(math.ceil(point_count / step)))
-        color_scale = offset_scale[3]
 
         for index in indices:
             start_offset = portion[0] + index * step
@@ -97,14 +94,14 @@ def run(
             else:
                 red = green = blue = np.zeros(num)
 
-            if not color_scale:
-                red = red.astype(np.uint8)
-                green = green.astype(np.uint8)
-                blue = blue.astype(np.uint8)
-            else:
+            if color_scale:
                 red = (red * color_scale).astype(np.uint8)
                 green = (green * color_scale).astype(np.uint8)
                 blue = (blue * color_scale).astype(np.uint8)
+            else:
+                red = red.astype(np.uint8)
+                green = green.astype(np.uint8)
+                blue = blue.astype(np.uint8)
 
             colors = np.vstack((red, green, blue)).transpose()
             colors = colors[start_offset : (start_offset + num)]

py3dtiles/reader/xyz_reader.py
@@ -12,9 +12,7 @@ from py3dtiles.typing import MetadataReaderType, OffsetScaleType, PortionType
 from py3dtiles.utils import ResponseType
 
 
-def get_metadata(
-    path: Path, color_scale: Optional[float] = None, fraction: int = 100
-) -> MetadataReaderType:
+def get_metadata(path: Path, fraction: int = 100) -> MetadataReaderType:
     aabb = None
     count = 0
     seek_values = []
@@ -70,7 +68,6 @@ def get_metadata(
     return {
         "portions": pointcloud_file_portions,
         "aabb": aabb,
-        "color_scale": color_scale,
         "srs_in": None,
         "point_count": point_count,
         "avg_min": aabb[0],
@@ -83,6 +80,7 @@ def run(
     portion: PortionType,
     queue: Socket,
     transformer: Optional[Transformer],
+    color_scale: Optional[float],
 ) -> None:
     """
     Reads points from a xyz file
@@ -145,7 +143,10 @@ def run(
                 coords = np.ascontiguousarray(coords.astype(np.float32))
 
                 # Read colors: 3 last columns of the point cloud
-                colors = points[:, -3:].astype(np.uint8)
+                if color_scale:
+                    colors = (points[:, -3:] * color_scale).astype(np.uint8)
+                else:
+                    colors = points[:, -3:].astype(np.uint8)
 
                 classification = np.zeros((points.shape[0], 1), dtype=np.uint8)
                 queue.send_multipart(