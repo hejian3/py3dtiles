fix(convert): fix pointcloud points position precision

py3dtiles/reader/ply_reader.py
@@ -84,7 +84,7 @@ def run(
                 # the inverse of this matrix)
                 coords = np.dot(coords, offset_scale[2])
 
-            coords = np.ascontiguousarray(coords.astype(np.float32))
+            coords = np.ascontiguousarray(coords.astype(np.float64))
 
             # Read colors
             if "red" in ply_vertices:

py3dtiles/reader/xyz_reader.py
@@ -106,7 +106,7 @@ def run(
             feature_nb = 7
 
             for _ in range(0, point_count, step):
-                points = np.zeros((step, feature_nb), dtype=np.float32)
+                points = np.zeros((step, feature_nb), dtype=np.float64)
 
                 for j in range(step):
                     line = f.readline()